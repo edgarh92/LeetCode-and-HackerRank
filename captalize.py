dbe3de1cd86c757ca5b6f764a609f2a8b5171109
Edgar Hernandez <edgarhernandez00@gmail.com>
2022-05-24 21:22:41 -0700

    [Capalize] Given a full name, your task is to capitalize the name appropriately.

---
+++ captalize.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def first_non_numeric(string):
+    for count, letter in enumerate(string):
+        if not letter.isnumeric():
+            return letter.upper(), count + 1
+def solve(s):
+    first,last = s.split(" ")
+    first_upper,first_splice_indice = first_non_numeric(first)
+    last_upper, last_splice_indice = first_non_numeric(last)
+    
+        
+            
+    first = first_upper + first[first_splice_indice:]
+    last = last_upper + last[last_splice_indice:]
+    capitalized = first + " " + last
+    return capitalized
+
+
+print(solve("123e2dgar 123hernande2z"))
