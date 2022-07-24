62c97afa346df7f7758a041389516ef623687da4
Edgar Hernandez <ehernandez@linkedin.com>
2022-07-23 22:07:51 -0700

    [Min Heap] [Two Sum]

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            print(n)
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
