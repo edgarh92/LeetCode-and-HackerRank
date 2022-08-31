ce8bfa629d25c4e88af2657ee32e2dc2db4504db
Edgar Hernandez <edgarhernandez00@gmail.com>
2022-08-31 07:37:08 -0700

    [Leetcode - Two Pointers] Contains Most Water

---
+++ contains_most_water.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        res = 0
+        
+        l, r = 0 , len(height) -1 
+        
+        while l < r:
+            area = (r - l ) * min(height[l], height[r]) # width * height
+            
+            res = max(res, area)
+            
+            if height[l] > height[r]:
+                r-=1
+            else:
+                l+=1
+        return res
+    
+    
+            
